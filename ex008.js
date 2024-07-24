e945daec402f22677f3141a1f13deb4f7367eee1
Melany Correa <melanycristinipereira@gmail.com>
2024-07-24 13:19:36 -0300

    Create ex008.js

---
+++ ex008.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log('A velocidade do seu carro é ${vel}km/h')
+if(vel > 60) {// condição simples
+    console.log('Você ultrapassou a velocidade permitida. Multado')
+}
+console.log('dirija sempre usando cinto de seguraça!')
